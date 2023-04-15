907c9dc616af1bcdbb06227e2ad09321104adfc2
Yavorskiy Maksim <maxfatal@mail.ru>
2023-04-15 20:28:58 +0300

    solved extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,11 +15,40 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+function repeater(str, options) {
+  obj = {
+    separator: '+',
+    additionSeparator: '|',
+    repeatTimes:1,
+    additionRepeatTimes:1,
+  }
+  Object.assign(obj, options)
 
-module.exports = {
-  repeater
-};
+  let mainString = []
+  let addition = []
+  let string = ''
+  if(obj.addition === null){
+    obj.addition = 'null'
+  }
+  if (str === null){
+    str = 'null'
+  }
+    for (let i = 0; i < obj.additionRepeatTimes; i++) {
+      addition.push(obj.addition);
+    }
+    addition = addition.join(obj.additionSeparator);
+    if(typeof str !== 'string'|| typeof addition !== 'string') {
+  string = str.toString() +  addition.toString()
+    }
+    string = str +  addition
+  for(let k=0;k< obj.repeatTimes; k++){
+    mainString.push(string)
+  }
+  mainString = mainString.join(obj.separator)
+return mainString
+}
+console.log(repeater(null, { repeatTimes: 3, separator: '??? ', addition: null, additionRepeatTimes: 3, additionSeparator: '!!!' }));
+
+  module.exports = {
+    repeater
+  };
